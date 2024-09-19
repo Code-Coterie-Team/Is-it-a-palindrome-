e1b1549c5b06de703c4b9586513fe4273283b526
Abolfazl <abtarahomii@gmail.com>
2024-09-19 16:42:27 +0330

    check code client

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 function isPalindrome(str) {
-    // نرمال‌سازی رشته: تبدیل به حروف کوچک و حذف کاراکترهای غیرالفبایی
+    //   تبدیل به حروف کوچک و حذف کاراکترهای غیرالفبایی
     const normalizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
     
     // معکوس کردن رشته نرمال‌شده
     const reversedStr = normalizedStr.split('').reverse().join('');
-    
-    // چک کردن اینکه آیا رشته نرمال‌شده برابر معکوس آن است
     return normalizedStr === reversedStr;
 }
 
